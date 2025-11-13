dbf2bdacc1c76a4f5e478d9b31774e73ba8a045a
Preet Chauhan <preetchauhan421@gmail.com>
2025-11-13 13:36:16 +0530

    adding dockerfile and cicd.yml

--- mongoC.js
+++ mongoC.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const connectionString = `mongodb+srv://preetchauhan_db_user:<db_password>@devcluster.cafuhtt.mongodb.net/?appName=DevCluster`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
 try {
@@ -10,5 +10,5 @@ try {
 } catch(e) {
   console.error(e);
 }
-let db = conn.db("integration_ninjas");
+let db = conn.db("preetchauhan_db");
 export default db;
